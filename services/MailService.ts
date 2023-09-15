5ff6db2e3c1414f83329c5b8795e55787d5b4930
Gedeon Ebamba <gedebamba3@gmail.com>
2023-09-15 08:39:08 +0100

    make auth config optional in nodemailer

--- services/MailService.ts
+++ services/MailService.ts
@@ -6,13 +6,28 @@ import { IMailPayload } from '../types/Mail';
 
 dotenv.config();
 
+function getAuthConfig() {
+  const authConfig: {
+    auth?: {
+      user: string,
+      pass: string,
+    }
+  } = {};
+
+  if (process.env.MAIL_USERNAME || process.env.MAIL_PASSWORD) {
+    authConfig.auth = {
+      user: process.env.MAIL_USERNAME as string,
+      pass: process.env.MAIL_PASSWORD as string,
+    };
+  }
+
+  return authConfig;
+}
+
 const TRANSPORT = nodemailer.createTransport({
   host: process.env.MAIL_HOST,
   port: process.env.MAIL_PORT as unknown as number,
-  auth: {
-    user: process.env.MAIL_USERNAME,
-    pass: process.env.MAIL_PASSWORD,
-  },
+  ...getAuthConfig(),
   tls: {
     rejectUnauthorized: false,
   },
