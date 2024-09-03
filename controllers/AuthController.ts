d76ca1ab9801ef26fe2d8ce04f3c521c5e7f408e
Gedeon Ebamba <gedebamba3@gmail.com>
2024-09-03 14:11:37 +0100

    change JWT time validity

--- controllers/AuthController.ts
+++ controllers/AuthController.ts
@@ -21,7 +21,7 @@ import UserService from '../services/UserService';
 
 const jwt = require('jsonwebtoken');
 
-const JWT_TIME_VALIDITY = 300000; // 5min
+const JWT_TIME_VALIDITY = 30 * 60; // 30 min
 const JWT_PASSWORD_TOKEN_TIME_VALIDITY = 5 * 60;
 const MAX_LOGIN_ATTEMPT = 3;
 
